d6b18b79a1c476c6d6f0dd429c9c3bda0f8e08fd
Andres Sainz <andresmsainz@gmail.com>
2022-03-27 00:56:28 -0400

    initial commit

---
+++ 51_Loops_Exercise/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _51_Loops_Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Loops Exercise!");
+
+            Console.WriteLine("Even numbers Loop!");
+            for (int i = 0; i <= 100; i+=2)
+            {
+                Console.Write(i +",");
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("Triangle Loop!");
+            string sline = "";
+            for (int i = 1; i<= 10; i++)
+            {
+                sline = sline + "0 ";
+                Console.WriteLine(sline);
+            }
+        }
+    }
+}
